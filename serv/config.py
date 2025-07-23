f6c84100ad5cea88ebb60e568ab5a626fe42e054
Talel Boussetta <talel.boussetta@insat.ucar.tn>
2025-07-23 18:07:22 +0100

    Updated config to use the env file not the api directly

--- serv/config.py
+++ serv/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
 
-HF_API_TOKEN = os.getenv("HF_API_TOKEN", "your_token_here")
+load_dotenv()  # load .env file variables into os.environ
+
+HF_API_TOKEN = os.getenv("HF_API_TOKEN")
 HF_MODEL_ENDPOINT = os.getenv("HF_MODEL_ENDPOINT", "https://api-inference.huggingface.co/models/<your-model>")
