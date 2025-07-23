affe370b0a3af0e352eba249495d11368d0b5b4b
Talel Boussetta <talel.boussetta@insat.ucar.tn>
2025-07-23 18:05:14 +0100

    Update config.py

---
+++ serv/config.py
@@ -0,0 +1,4 @@
+import os
+
+HF_API_TOKEN = os.getenv("HF_API_TOKEN", "your_token_here")
+HF_MODEL_ENDPOINT = os.getenv("HF_MODEL_ENDPOINT", "https://api-inference.huggingface.co/models/<your-model>")
