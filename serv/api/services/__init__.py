78c1f5a7b8416193737a1d14c04bb4f54690df8f
Talel Boussetta <talel.boussetta@insat.ucar.tn>
2026-01-20 10:26:53 +0100

    added init file for package management for the services folder that define the core of our business logic layer

--- serv/api/services/__init__.py
+++ serv/api/services/__init__.py
@@ -1,3 +1,21 @@
 """
-FocusGuard API Services Package
+FocusGuard API - Services Package
+
+Business logic layer for the FocusGuard API.
 """
+
+from . import (
+    auth_service,
+    user_service,
+    session_service,
+    garden_service,
+    stats_service
+)
+
+__all__ = [
+    "auth_service",
+    "user_service",
+    "session_service",
+    "garden_service",
+    "stats_service"
+]
