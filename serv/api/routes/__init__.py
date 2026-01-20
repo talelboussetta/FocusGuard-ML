df23b49fc3c344b844905e0a5b696c903119cdca
Talel Boussetta <talel.boussetta@insat.ucar.tn>
2026-01-20 11:25:07 +0100

    added package manager file

--- serv/api/routes/__init__.py
+++ serv/api/routes/__init__.py
@@ -1,3 +1,19 @@
 """
-FocusGuard API Routes Package
+FocusGuard API - Routes Package
+
+HTTP endpoints for the FocusGuard API.
 """
+
+from .auth import router as auth_router
+from .users import router as users_router
+from .sessions import router as sessions_router
+from .garden import router as garden_router
+from .stats import router as stats_router
+
+__all__ = [
+    "auth_router",
+    "users_router",
+    "sessions_router",
+    "garden_router",
+    "stats_router"
+]
